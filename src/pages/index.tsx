7cd29b1443754714accaae04c5e3e92e9c2af51d
Brad Niepceron <brad.niepce@gmail.com>
2023-01-13 14:29:43 +0100

    Add sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,7 @@
-import { signOut, useSession } from "next-auth/react";
-import { useRouter } from 'next/dist/client/router';
+import Layout from "@/components/layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-  const router = useRouter();
-
   return (
-    <>
-      <main>
-        {session ? (
-            <button onClick={() => signOut()}>Log out</button>
-          ) : (
-            <button
-              onClick={() => {
-                router.push("/api/auth/signin");
-              }}
-            >
-              Sign in
-            </button>
-        )}
-      </main>
-    </>
+    <Layout></Layout>
   )
 }
